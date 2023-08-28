caf03b87db3bf11c947b4349659e1f45b7caf236
Anastasia Haller <anadana87@gmail.com>
2023-08-28 12:58:04 +0200

    add email input and validation

--- src/components/SomeInput.js
+++ src/components/SomeInput.js
@@ -4,12 +4,18 @@ const SomeInput = (props) => {
 
   const [enteredName, setEnteredName] = useState("");
   const [wasNameInputTouched, setWasNameInputTouched] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [wasEmailInputTouched, setWasEmailInputTouched] = useState(false);
 
   const isEnteredNameValid = enteredName.trim() !== '';
   const isNameInputInvalid = !isEnteredNameValid && wasNameInputTouched;
+
+  const isEnteredEmailValid = enteredEmail.includes('@');
+  const isEmailInputInvalid = !isEnteredEmailValid && wasEmailInputTouched;
+
   let isFormValid = false;
 
-  if (isEnteredNameValid) {
+  if (isEnteredNameValid && isEnteredEmailValid) {
     isFormValid = true;
   }
 
@@ -19,6 +25,14 @@ const SomeInput = (props) => {
 
   const nameInputLostFocusHandler = (event) => {
     setWasNameInputTouched(true);
+  };
+
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value);
+  };
+
+  const emailInputLostFocusHandler = (event) => {
+    setWasEmailInputTouched(true);
   };
 
   const formSubmitHandler = (event) => {
@@ -31,11 +45,17 @@ const SomeInput = (props) => {
     }
 
     console.log(enteredName);
+    console.log(enteredEmail);
+
     setEnteredName('');
     wasNameInputTouched(false);
+
+    setEnteredEmail('');
+    wasEmailInputTouched(false);
   };
 
   const nameInputClasses = !isNameInputInvalid ? 'form-control' : 'form-control invalid';
+  const emailInputClasses = !isEmailInputInvalid ? 'form-control' : 'form-control invalid';
 
   return (
     <form onSubmit={formSubmitHandler}>
@@ -49,6 +69,17 @@ const SomeInput = (props) => {
           value={enteredName}
         />
         {isNameInputInvalid && <p className="error-text">Enter Your Name</p>}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor="email">Your Email</label>
+        <input
+          type="email"
+          id="email"
+          onChange={emailInputChangeHandler}
+          onBlur={emailInputLostFocusHandler}
+          value={enteredEmail}
+        />
+        {isEmailInputInvalid && <p className="error-text">Enter Your Email</p>}
       </div>
       <div className="form-actions">
         <button disabled={!isFormValid}>Send</button>
